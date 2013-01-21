9c73dadab0139956f395647f5f7ee32b6255adb3
Hagen Schink <hagen.schink@gmail.com>
2013-01-21 19:42:39 +0100

    Test Drop Column Refactoring

--- test/org/iti/sqlSchemaComparison/frontends/SqliteSchemaFrontendTest.java
+++ test/org/iti/sqlSchemaComparison/frontends/SqliteSchemaFrontendTest.java
@@ -6,6 +6,8 @@ import java.util.Set;
 
 import junit.framework.Assert;
 
+import org.iti.sqlSchemaComparison.SqlSchemaComparer;
+import org.iti.sqlSchemaComparison.SqlSchemaComparisonResult;
 import org.iti.sqlSchemaComparison.edge.ForeignKeyRelationEdge;
 import org.iti.sqlSchemaComparison.vertex.ISqlElement;
 import org.iti.sqlSchemaComparison.vertex.SqlColumnVertex;
@@ -25,6 +27,9 @@ import org.junit.runners.JUnit4;
 public class SqliteSchemaFrontendTest {
 
 	private static final String DATABASE_FILE_PATH = "test\\databases\\hrm.sqlite";
+	private static final String DROPPED_COLUMN_DATABASE_FILE_PATH = "test\\databases\\refactored\\hrm_DropColumn.sqlite";
+	
+	private static final String DROPPED_COLUMN_NAME = "boss";
 	
 	@Before
 	public void setUp() { }
@@ -69,6 +74,21 @@ public class SqliteSchemaFrontendTest {
 		}
 		
 		return columns;
+	}
+	
+	@Test
+	public void DroppedColumnDetectedCorrectly() {
+		ISqlSchemaFrontend frontend1 = new SqliteSchemaFrontend(DATABASE_FILE_PATH);
+		ISqlSchemaFrontend frontend2 = new SqliteSchemaFrontend(DROPPED_COLUMN_DATABASE_FILE_PATH);
+		Graph<ISqlElement, DefaultEdge> schema1 = frontend1.createSqlSchema();
+		Graph<ISqlElement, DefaultEdge> schema2 = frontend2.createSqlSchema();
+		SqlSchemaComparer comparer = new SqlSchemaComparer(schema1, schema2);
+		SqlSchemaComparisonResult result = comparer.comparisonResult;
+		
+		Assert.assertEquals(31, SqlElementFactory.getSqlElementsOfType(SqlElementType.Column, schema1.vertexSet()).size());
+		Assert.assertEquals(30, SqlElementFactory.getSqlElementsOfType(SqlElementType.Column, schema2.vertexSet()).size());
+		Assert.assertNotNull(result.getRemovedColumn());
+		Assert.assertEquals(DROPPED_COLUMN_NAME, result.getRemovedColumn().getSqlElementId());
 	}
 
 	@After
