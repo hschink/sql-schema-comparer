cafb9026a02430a4c1ba5c46077f113220d6abf5
Hagen Schink <hagen.schink@gmail.com>
2013-08-14 18:37:05 +0200

    Remove superfluous fields column comparison result

--- src/main/java/org/iti/sqlSchemaComparison/SqlSchemaColumnComparisonResult.java
+++ src/main/java/org/iti/sqlSchemaComparison/SqlSchemaColumnComparisonResult.java
@@ -8,18 +8,6 @@ public class SqlSchemaColumnComparisonResult {
 
 	public static class ColumnConstraintComparisonResult {
 		
-		private List<IColumnConstraint> constraints1;
-		
-		public List<IColumnConstraint> getConstraints1() {
-			return constraints1;
-		}
-
-		private List<IColumnConstraint> constraints2;
-
-		public List<IColumnConstraint> getConstraints2() {
-			return constraints2;
-		}
-		
 		private List<IColumnConstraint> addedConstraints;
 
 		public List<IColumnConstraint> getAddedConstraints() {
@@ -32,13 +20,9 @@ public class SqlSchemaColumnComparisonResult {
 			return removedConstraints;
 		}
 		
-		public ColumnConstraintComparisonResult(List<IColumnConstraint> constraints1,
-				List<IColumnConstraint> constraints2,
-				List<IColumnConstraint> addedConstraints,
+		public ColumnConstraintComparisonResult(List<IColumnConstraint> addedConstraints,
 				List<IColumnConstraint> removedConstraints) {
 			
-			this.constraints1 = constraints1;
-			this.constraints2 = constraints2;
 			this.addedConstraints = addedConstraints;
 			this.removedConstraints = removedConstraints;
 		}
