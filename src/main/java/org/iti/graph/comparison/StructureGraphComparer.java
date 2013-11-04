5f31d89787b5332d17d931bb0c1d3d888648f439
Hagen Schink <hagen.schink@gmail.com>
2013-11-04 22:55:08 +0100

    Add sophisticated structure graph comparer

---
+++ src/main/java/org/iti/graph/comparison/StructureGraphComparer.java
@@ -0,0 +1,161 @@
+/*
+ *  Copyright 1999 Hagen Schink <hagen.schink@gmail.com>
+ *
+ *  This file is part of sql-schema-comparer.
+ *
+ *  sql-schema-comparer is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Lesser General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  sql-schema-comparer is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public License
+ *  along with sql-schema-comparer.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ *
+ */
+
+package org.iti.graph.comparison;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+
+import org.iti.graph.IStructureGraph;
+import org.iti.graph.nodes.IStructureElement;
+
+public class StructureGraphComparer implements IStructureGraphComparer {
+
+	public static class AmbiguousRenameException extends StructureGraphComparisonException {
+		private static final long serialVersionUID = -3176377321899125075L;
+	}
+
+	public static class AmbiguousMoveException extends StructureGraphComparisonException {
+		private static final long serialVersionUID = -3666070878704536627L;
+	}
+
+	private StructureGraphComparisonResult result;
+
+	private IStructureGraph oldGraph;
+	private IStructureGraph newGraph;
+
+	private Map<String, List<IStructureElement>> removedNodesByPath;
+	private Map<String, List<IStructureElement>> addedNodesByPath;
+
+	@Override
+	public StructureGraphComparisonResult compare(IStructureGraph oldGraph,
+			IStructureGraph newGraph) throws StructureGraphComparisonException {
+		SimpleStructureGraphComparer simpleStructureGraphComparer = new SimpleStructureGraphComparer();
+
+		result = simpleStructureGraphComparer.compare(oldGraph, newGraph);
+
+		this.oldGraph = oldGraph;
+		this.newGraph = newGraph;
+
+		groupAddedAndRemovedNodesByPath();
+
+		setRenamedNodes();
+		setMovedNodes();
+
+		return result;
+	}
+
+	private void groupAddedAndRemovedNodesByPath() {
+		Collection<IStructureElement> removedNodes = result.getElementsByModification(StructureElementModification.NodeDeleted);
+		Collection<IStructureElement> addedNodes = result.getElementsByModification(StructureElementModification.NodeAdded);
+
+		removedNodesByPath = getNodesByPath(oldGraph, removedNodes);
+		addedNodesByPath = getNodesByPath(newGraph, addedNodes);
+	}
+
+	private static Map<String, List<IStructureElement>> getNodesByPath(
+			IStructureGraph graph,
+			Collection<IStructureElement> elements) {
+		Map<String, List<IStructureElement>> missingNodesByPath = new HashMap<>();
+
+		for (IStructureElement element : elements) {
+			String path = graph.getPath(element);
+
+			if (!missingNodesByPath.containsKey(path)) {
+				missingNodesByPath.put(path, new ArrayList<IStructureElement>());
+			}
+
+			missingNodesByPath.get(path).add(element);
+		}
+
+		return missingNodesByPath;
+	}
+
+	private void setRenamedNodes() throws AmbiguousRenameException {
+		for (Entry<String, List<IStructureElement>> removedInPath : removedNodesByPath.entrySet()) {
+			for (IStructureElement removedElement : removedInPath.getValue()) {
+				IStructureElement renamedElement = findRenamedElement(removedInPath.getKey(), removedElement);
+
+				exchangeNode(removedElement, renamedElement, StructureElementModification.NodeRenamed);
+			}
+		}
+	}
+
+	private IStructureElement findRenamedElement(String path,
+			IStructureElement removedElement) throws AmbiguousRenameException {
+		List<IStructureElement> addedElementsInPath = addedNodesByPath.get(path);
+
+		if (addedElementsInPath != null) {
+			switch (addedElementsInPath.size()) {
+				case 0: return null;
+				case 1:
+					IStructureElement addedElement = addedElementsInPath.get(0);
+
+					return addedElement;
+				default: throw new AmbiguousRenameException();
+			}
+		}
+
+		return null;
+	}
+
+	private void exchangeNode(IStructureElement removedElement,
+			IStructureElement addedElement,
+			StructureElementModification modification) {
+		if (addedElement != null) {
+			result.removeModification(oldGraph.getIdentifier(removedElement));
+			result.removeModification(newGraph.getIdentifier(addedElement));
+
+			result.addModification(result.getNewGraph().getIdentifier(addedElement), modification);
+		}
+	}
+
+	private void setMovedNodes() throws AmbiguousMoveException {
+		for (Entry<String, List<IStructureElement>> removedInPath : removedNodesByPath.entrySet()) {
+			for (IStructureElement removedElement : removedInPath.getValue()) {
+				IStructureElement movedElement = findMovedElement(removedElement);
+				
+				exchangeNode(removedElement, movedElement, StructureElementModification.NodeMoved);
+			}
+		}
+	}
+
+	private IStructureElement findMovedElement(IStructureElement element) throws AmbiguousMoveException {
+		Collection<IStructureElement> addedElements;
+		
+		addedElements = result.getElementsByIdentifier(element.getIdentifier(), StructureElementModification.NodeAdded);
+
+		switch (addedElements.size()) {
+			case 0: return null;
+		
+			case 1: 
+				IStructureElement addedElement = addedElements.iterator().next();
+
+				return addedElement;
+
+			default: throw new AmbiguousMoveException();
+		}
+	}
+}
---
+++ src/test/java/org/iti/graph/comparison/StructureGraphComparerTest.java
@@ -0,0 +1,281 @@
+/*
+ *  Copyright 1999 Hagen Schink <hagen.schink@gmail.com>
+ *
+ *  This file is part of sql-schema-comparer.
+ *
+ *  sql-schema-comparer is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Lesser General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  sql-schema-comparer is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public License
+ *  along with sql-schema-comparer.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ *
+ */
+
+package org.iti.graph.comparison;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.iti.graph.StructureGraph;
+import org.iti.graph.comparison.StructureGraphComparer.AmbiguousMoveException;
+import org.iti.graph.comparison.StructureGraphComparer.AmbiguousRenameException;
+import org.iti.graph.helper.Edge1;
+import org.iti.graph.helper.Edge2;
+import org.iti.graph.helper.Edge3;
+import org.iti.graph.helper.Edge4;
+import org.iti.graph.helper.Edge5;
+import org.iti.graph.helper.Edge6;
+import org.iti.graph.helper.Edge7;
+import org.iti.graph.helper.Edge8;
+import org.iti.graph.helper.Element;
+import org.iti.graph.nodes.IStructureElement;
+import org.jgrapht.DirectedGraph;
+import org.jgrapht.graph.DefaultEdge;
+import org.jgrapht.graph.SimpleDirectedGraph;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class StructureGraphComparerTest {
+
+	private static IStructureGraphComparer comparer;
+
+	private static StructureGraph structureGraphOriginal;
+	
+	private static Element re = new Element("re");
+	private static Element cn1 = new Element("cn1");
+	private static Element cn2 = new Element("cn2");
+	private static Element cn3 = new Element("cn3");
+	private static Element cn4 = new Element("cn4");
+	private static Element cn5 = new Element("cn5");
+	private static Element cn6 = new Element("cn6");
+	private static Element cn7 = new Element("cn7");
+	private static Element cn8 = new Element("cn8");
+	
+	private static DirectedGraph<IStructureElement, DefaultEdge> originalGraph;
+
+	private DirectedGraph<IStructureElement, DefaultEdge> currentGraph;
+	
+	@BeforeClass
+	public static void init() throws Exception {
+		comparer = new StructureGraphComparer();
+
+		originalGraph = new SimpleDirectedGraph<IStructureElement, DefaultEdge>(DefaultEdge.class);
+		
+		originalGraph.addVertex(re);
+		originalGraph.addVertex(cn1);
+		originalGraph.addVertex(cn2);
+		originalGraph.addVertex(cn3);
+		originalGraph.addVertex(cn4);
+		originalGraph.addVertex(cn5);
+		originalGraph.addVertex(cn6);
+		
+		originalGraph.addEdge(re, cn1, new Edge1());
+		originalGraph.addEdge(cn1, cn3, new Edge2());
+		originalGraph.addEdge(cn1, cn4, new Edge3());
+		originalGraph.addEdge(re, cn2, new Edge4());
+		originalGraph.addEdge(cn2, cn5, new Edge5());
+		originalGraph.addEdge(cn2, cn6, new Edge6());
+
+		structureGraphOriginal = new StructureGraph(originalGraph);
+	}
+
+	@Before
+	public void setUp() throws Exception {
+		currentGraph = new SimpleDirectedGraph<IStructureElement, DefaultEdge>(DefaultEdge.class);
+
+		currentGraph.addVertex(re);
+		currentGraph.addVertex(cn1);
+		currentGraph.addVertex(cn2);
+		currentGraph.addVertex(cn3);
+		currentGraph.addVertex(cn4);
+		currentGraph.addVertex(cn5);
+		currentGraph.addVertex(cn6);
+		
+		currentGraph.addEdge(re, cn1, new Edge1());
+		currentGraph.addEdge(cn1, cn3, new Edge2());
+		currentGraph.addEdge(cn1, cn4, new Edge3());
+		currentGraph.addEdge(re, cn2, new Edge4());
+		currentGraph.addEdge(cn2, cn5, new Edge5());
+		currentGraph.addEdge(cn2, cn6, new Edge6());
+	}
+
+	@Test
+	public void detectsRemovedNodes() throws StructureGraphComparisonException {
+		Map<String, StructureElementModification> expectedModifications = new HashMap<>();
+		expectedModifications.put(structureGraphOriginal.getIdentifier(cn3), StructureElementModification.NodeDeleted);
+		expectedModifications.put(structureGraphOriginal.getIdentifier(cn2), StructureElementModification.NodeDeleted);
+		expectedModifications.put(structureGraphOriginal.getIdentifier(cn5), StructureElementModification.NodeDeleted);
+		expectedModifications.put(structureGraphOriginal.getIdentifier(cn6), StructureElementModification.NodeDeleted);
+
+		currentGraph.removeVertex(cn3);
+		currentGraph.removeVertex(cn2);
+		currentGraph.removeVertex(cn5);
+		currentGraph.removeVertex(cn6);
+
+		StructureGraph structureGraphCurrent = new StructureGraph(currentGraph);
+
+		StructureGraphComparisonResult result = comparer.compare(structureGraphOriginal, structureGraphCurrent);
+
+		assertEquals(expectedModifications.size(), result.getModifications().size());
+
+		StructureGraphComparerTestHelper.assertNodeModificationExpectations(expectedModifications, result);
+	}
+
+	@Test
+	public void detectsAddedNodes() throws StructureGraphComparisonException {
+		currentGraph.addVertex(cn7);
+		currentGraph.addVertex(cn8);
+
+		currentGraph.addEdge(cn1, cn7, new Edge7());
+		currentGraph.addEdge(re, cn8, new Edge8());
+
+		StructureGraph structureGraphCurrent = new StructureGraph(currentGraph);
+
+		Map<String, StructureElementModification> expectedModifications = new HashMap<>();
+		expectedModifications.put(structureGraphCurrent.getIdentifier(cn7), StructureElementModification.NodeAdded);
+		expectedModifications.put(structureGraphCurrent.getIdentifier(cn8), StructureElementModification.NodeAdded);
+
+		StructureGraphComparisonResult result = comparer.compare(structureGraphOriginal, structureGraphCurrent);
+
+		assertEquals(expectedModifications.size(), result.getModifications().size());
+
+		StructureGraphComparerTestHelper.assertNodeModificationExpectations(expectedModifications, result);
+	}
+
+	@Test
+	public void detectsAddedAndRemovedNodes() throws StructureGraphComparisonException {
+		currentGraph.removeVertex(cn3);
+		currentGraph.removeVertex(cn2);
+		currentGraph.removeVertex(cn5);
+		currentGraph.removeVertex(cn6);
+
+		currentGraph.addVertex(cn7);
+		currentGraph.addVertex(cn8);
+
+		currentGraph.addEdge(cn1, cn7, new Edge7());
+		currentGraph.addEdge(re, cn8, new Edge8());
+
+		StructureGraph structureGraphCurrent = new StructureGraph(currentGraph);
+
+		Map<String, StructureElementModification> expectedModifications = new HashMap<>();
+		expectedModifications.put(structureGraphOriginal.getIdentifier(cn3), StructureElementModification.NodeDeleted);
+		expectedModifications.put(structureGraphOriginal.getIdentifier(cn2), StructureElementModification.NodeDeleted);
+		expectedModifications.put(structureGraphOriginal.getIdentifier(cn5), StructureElementModification.NodeDeleted);
+		expectedModifications.put(structureGraphOriginal.getIdentifier(cn6), StructureElementModification.NodeDeleted);
+		expectedModifications.put(structureGraphCurrent.getIdentifier(cn7), StructureElementModification.NodeAdded);
+		expectedModifications.put(structureGraphCurrent.getIdentifier(cn8), StructureElementModification.NodeAdded);
+
+		StructureGraphComparisonResult result = comparer.compare(structureGraphOriginal, structureGraphCurrent);
+
+		assertEquals(expectedModifications.size(), result.getModifications().size());
+
+		StructureGraphComparerTestHelper.assertNodeModificationExpectations(expectedModifications, result);
+	}
+
+	@Test
+	public void detectsRenamedNodes() throws StructureGraphComparisonException {
+		Element renamedElement = new Element("cn3r");
+
+		currentGraph.removeVertex(cn3);
+		currentGraph.addVertex(renamedElement);
+
+		currentGraph.addEdge(cn1, renamedElement, new Edge2());
+
+		StructureGraph structureGraphCurrent = new StructureGraph(currentGraph);
+
+		Map<String, StructureElementModification> expectedModifications = new HashMap<>();
+		expectedModifications.put(structureGraphCurrent.getIdentifier(renamedElement), StructureElementModification.NodeRenamed);
+
+		StructureGraphComparisonResult result = comparer.compare(structureGraphOriginal, structureGraphCurrent);
+
+		assertEquals(expectedModifications.size(), result.getModifications().size());
+
+		StructureGraphComparerTestHelper.assertNodeModificationExpectations(expectedModifications, result);
+	}
+
+	@Test
+	public void detectsMovedNodes() throws StructureGraphComparisonException {
+		currentGraph.removeEdge(cn2, cn6);
+
+		currentGraph.addEdge(cn4, cn6, new Edge6());
+
+		StructureGraph structureGraphCurrent = new StructureGraph(currentGraph);
+
+		Map<String, StructureElementModification> expectedModifications = new HashMap<>();
+		expectedModifications.put(structureGraphCurrent.getIdentifier(cn6), StructureElementModification.NodeMoved);
+
+		StructureGraphComparisonResult result = comparer.compare(structureGraphOriginal, structureGraphCurrent);
+
+		assertEquals(expectedModifications.size(), result.getModifications().size());
+
+		StructureGraphComparerTestHelper.assertNodeModificationExpectations(expectedModifications, result);
+	}
+
+	@Test
+	public void detectsRenamedMovedNodes() throws StructureGraphComparisonException {
+		Element renamedElement = new Element("cn3r");
+
+		currentGraph.removeEdge(cn2, cn6);
+		currentGraph.removeVertex(cn3);
+		currentGraph.addVertex(renamedElement);
+
+		currentGraph.addEdge(cn1, renamedElement, new Edge2());
+		currentGraph.addEdge(cn4, cn6, new Edge6());
+
+		StructureGraph structureGraphCurrent = new StructureGraph(currentGraph);
+
+		Map<String, StructureElementModification> expectedModifications = new HashMap<>();
+		expectedModifications.put(structureGraphCurrent.getIdentifier(cn6), StructureElementModification.NodeMoved);
+		expectedModifications.put(structureGraphCurrent.getIdentifier(renamedElement), StructureElementModification.NodeRenamed);
+
+		StructureGraphComparisonResult result = comparer.compare(structureGraphOriginal, structureGraphCurrent);
+
+		assertEquals(expectedModifications.size(), result.getModifications().size());
+
+		StructureGraphComparerTestHelper.assertNodeModificationExpectations(expectedModifications, result);
+	}
+	
+	@Test(expected=AmbiguousRenameException.class)
+	public void throwsAmbiguousRenameException() throws StructureGraphComparisonException {
+		Element renamedElement1 = new Element("cn3r1");
+		Element renamedElement2 = new Element("cn3r2");
+
+		currentGraph.removeVertex(cn3);
+		currentGraph.addVertex(renamedElement1);
+		currentGraph.addVertex(renamedElement2);
+
+		currentGraph.addEdge(cn1, renamedElement1, new Edge2());
+		currentGraph.addEdge(cn1, renamedElement2, new Edge2());
+
+		StructureGraph structureGraphCurrent = new StructureGraph(currentGraph);
+
+		comparer.compare(structureGraphOriginal, structureGraphCurrent);
+	}
+
+	@Test(expected=AmbiguousMoveException.class)
+	public void throwsAmbiguousMoveException() throws StructureGraphComparisonException {
+		Element movedElement = new Element("cn6");
+
+		currentGraph.addVertex(movedElement);
+
+		currentGraph.removeEdge(cn2, cn6);
+
+		currentGraph.addEdge(cn4, cn6, new Edge6());
+		currentGraph.addEdge(cn3, movedElement, new Edge6());
+
+		StructureGraph structureGraphCurrent = new StructureGraph(currentGraph);
+
+		comparer.compare(structureGraphOriginal, structureGraphCurrent);
+	}
+}
